a6231401e282895d9a4d4ec27ae8f4fd96db634e
Rkey83 <krame407@umn.edu>
2014-08-05 17:12:43 -0500

    test

--- app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Adapters/CircularPagerAdapter.java
+++ app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Adapters/CircularPagerAdapter.java
@@ -3,18 +3,24 @@ package com.LocallyGrownStudios.liquidcommunications.Adapters;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.LocallyGrownStudios.liquidcommunications.Fragments.LiquidContactsFragment;
 import com.LocallyGrownStudios.liquidcommunications.Fragments.LiquidTextFragment;
 import com.LocallyGrownStudios.liquidcommunications.Fragments.QuickConnectFragment;
+import com.LocallyGrownStudios.liquidcommunications.R;
 
 public class CircularPagerAdapter extends FragmentPagerAdapter {
 
     Fragment currentFragment;
+    private int pos;
 
 
     public CircularPagerAdapter (FragmentManager fm) {
+
+
 
         super(fm);
 
@@ -23,8 +29,23 @@ public class CircularPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+
+        ViewPager viewPager = (ViewPager) container.findViewById(R.id.pager);
+        int i = viewPager.getCurrentItem();
+
+        if ( i < 1 ){
+            viewPager.setCurrentItem(3);
+        }
+        if ( i > 3){
+            viewPager.setCurrentItem(1);
+        }
+
         return super.instantiateItem(container, position);
+
+
     }
+
+
 
     @Override
     public Fragment getItem(int position) {
@@ -33,27 +54,37 @@ public class CircularPagerAdapter extends FragmentPagerAdapter {
 
             case 0:
 
-                currentFragment = LiquidContactsFragment.newInstance(position + 1);
+                currentFragment = LiquidTextFragment.newInstance(1);
                 return currentFragment;
 
             case 1:
 
-                currentFragment = QuickConnectFragment.newInstance(position + 1);
+                currentFragment = LiquidContactsFragment.newInstance(2);
                 return currentFragment;
 
             case 2:
 
-                currentFragment = LiquidTextFragment.newInstance(position + 1);
+                currentFragment = QuickConnectFragment.newInstance(3);
+                return currentFragment;
+
+            case 3:
+
+                currentFragment = LiquidTextFragment.newInstance(4);
+                return currentFragment;
+
+            case 4:
+
+                currentFragment = LiquidContactsFragment.newInstance(5);
                 return currentFragment;
 
         }
 
-        return null;
+        return LiquidContactsFragment.newInstance(position);
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return 5;
     }
 
     @Override
