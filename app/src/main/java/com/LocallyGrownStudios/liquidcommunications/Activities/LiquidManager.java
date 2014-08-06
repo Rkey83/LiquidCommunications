263e1846eb3bf75f3c57f44a34d268d8070fa863
LuckyLips <norda037@umn.edu>
2014-08-06 10:09:11 -0500

    Mark - Header arrows coorected with rotation

--- app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/LiquidManager.java
+++ app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/LiquidManager.java
@@ -14,8 +14,6 @@ import android.util.Log;
 import com.LocallyGrownStudios.liquidcommunications.Adapters.CircularPagerAdapter;
 import com.LocallyGrownStudios.liquidcommunications.ContentProviders.ContactProvider;
 import com.LocallyGrownStudios.liquidcommunications.R;
-import com.LocallyGrownStudios.liquidcommunications.Services.ContactService;
-import com.LocallyGrownStudios.liquidcommunications.Services.ImportContactService;
 import com.LocallyGrownStudios.liquidcommunications.Services.SmsMmsService;
 
 import static android.support.v4.view.ViewPager.*;
@@ -23,10 +21,10 @@ import static android.support.v4.view.ViewPager.*;
 
 public class LiquidManager extends Activity {
 
-    private int previousState;
+    private int previousState, currentState;
     private ViewPager mainPager;
     CircularPagerAdapter testAdapter;
-
+    Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle position) {
@@ -38,9 +36,15 @@ public class LiquidManager extends Activity {
         mainPager.setAdapter(testAdapter);
         mainPager.setCurrentItem(2);
         mainPager.setOnPageChangeListener(onPageChangeListener);
-        Intent serviceImportContacts = new Intent(this, ContactService.class);
-        startService(serviceImportContacts);
         Intent serviceSmsMms = new Intent(context, SmsMmsService.class);
+        Cursor cursor = getContentResolver().query(ContactProvider.contactUri, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            String avb = cursor.getString(1);
+            String tyb = cursor.getString(5);
+            String kfd = cursor.getString(3);
+        }
+
         context.startService(serviceSmsMms);
 
 
@@ -60,16 +64,12 @@ public class LiquidManager extends Activity {
                 Log.e("onPageScrolled", "pageSelected" + pageSelected + ",positionOffset:" + positionOffset + ",positionOffsetPixel:" + positionOffsetPixel);
                 previousState = mainPager.getCurrentItem();
 
-               if (previousState == 0 && positionOffset < .001){
-
-                   mainPager.setCurrentItem(3, false);
+               if (previousState == 0 && positionOffset < .99){
+                   mainPager.setCurrentItem(3);
                }
 
-               if (previousState == 4 && positionOffset > .999){
-
-
-                   mainPager.setCurrentItem(1, false);
-
+               if (previousState == 4 && positionOffset > .01){
+                   mainPager.setCurrentItem(1);
                }
 
             }
@@ -77,6 +77,15 @@ public class LiquidManager extends Activity {
             @Override
             public void onPageScrollStateChanged(int state) {
                 Log.e("onPageScrollStateChanged", "state:" + state);
+
+          //      currentState = mainPager.getCurrentItem();
+
+         //       if (previousState == 1 && currentState == 0){
+          //          mainPager.setCurrentItem(3);
+          //      }
+         //       if (previousState == 3 && currentState == 4){
+         //           mainPager.setCurrentItem(1);
+           //     }
 
             }
         };
