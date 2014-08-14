298be82edc83316443bf3ff07db52695a8af102a
Rkey83 <krame407@umn.edu>
2014-08-14 09:35:09 -0500

    clean up

--- app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/FooterContainer.java
+++ app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/FooterContainer.java
@@ -10,36 +10,15 @@ import android.view.ViewGroup;
 
 import com.LocallyGrownStudios.liquidcommunications.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FooterContainer.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FooterContainer#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+
 public class FooterContainer extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FooterContainer.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FooterContainer newInstance(String param1, String param2) {
         FooterContainer fragment = new FooterContainer();
         Bundle args = new Bundle();
@@ -48,6 +27,7 @@ public class FooterContainer extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
     public FooterContainer() {
         // Required empty public constructor
     }
@@ -81,8 +61,7 @@ public class FooterContainer extends Fragment {
         try {
             mListener = (OnFragmentInteractionListener) activity;
         } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new ClassCastException(activity.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -92,16 +71,6 @@ public class FooterContainer extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
