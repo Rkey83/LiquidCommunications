ef3ec3f5e77b2f224bacd4c508ba065a3bfe4313
Rkey83 <krame407@umn.edu>
2014-08-13 21:33:52 -0500

    check 1

--- app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/LiquidManager.java
+++ app/src/main/java/com/LocallyGrownStudios/liquidcommunications/Activities/LiquidManager.java
@@ -74,5 +74,15 @@ public class LiquidManager extends Activity {
 
             }
         };
+
+     public void onBackPressed() {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+
+       }
 }
 
